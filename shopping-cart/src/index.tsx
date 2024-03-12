5e7293a7a40f6561aecf8d92bf95cf7eb0cbe0db
Corey Clark <coreychristianclark@gmail.com>
2024-03-12 13:08:13 -0400

    Revert "Lots of imports. Switched setup from ReactDOM to createRoot"
    This reverts commit 1401256d0d5a5659fcfd66107164351a0f06f9ab.

--- shopping-cart/src/index.tsx
+++ shopping-cart/src/index.tsx
@@ -1,22 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import { QueryClient, QueryClientProvider } from "react-query";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import {QueryClient, QueryClientProvider} from 'react-query'
 
 const client = new QueryClient();
 
-const container = document.getElementById("root")!; // The '!' tells TypeScript that we are positive that document.getElementById('root') will not return 'null'.
-const root = ReactDOM.createRoot(container);
-
-root.render(
+ReactDOM.render(
   <QueryClientProvider client={client}>
     <App />
-  </QueryClientProvider>
-);
-
-// ReactDOM.render(
-//   <QueryClientProvider client={client}>
-//     <App />
-//   </QueryClientProvider>,
-//   document.getElementById('root')
-// )
+  </QueryClientProvider>,
+  document.getElementById('root')
+)
