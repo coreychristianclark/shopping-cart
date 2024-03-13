e87320e0f1026de8ad62552d29bd1053e75ecb6e
Corey Clark <coreychristianclark@gmail.com>
2024-03-13 09:36:09 -0400

    Updated package dependencies for Material UI

--- shopping-cart/src/App.tsx
+++ shopping-cart/src/App.tsx
@@ -2,12 +2,12 @@ import { useState } from "react";
 import { useQuery } from "react-query";
 // Components
 // import Drawer from "@material-ui/core/Drawer";
-// import LinearProgress from "@material-ui/core/LinearProgress";
+import LinearProgress from "@mui/material/LinearProgress";
 // import Grid from "@material-ui/core/Grid";
 // import AddShoppingCardIcon from "@material-ui/icons/AddShoppingCart";
 // import Badge from "@material-ui/core/Badge";
 // Styles
-import { Wrapper } from './App.styles';
+import { Wrapper } from "./App.styles";
 // Types
 export type CartItemType = {
   id: number;
@@ -24,10 +24,20 @@ const getProducts = async (): Promise<CartItemType[]> => // Promises are generic
 
 const App = () => {
   const { data, isLoading, error } = useQuery<CartItemType[]>(
-    "products",
+    "products", // 'products' is our query key.
     getProducts
   );
-console.log(data)
+  console.log(data);
+
+  const getTotalItems = () => null;
+
+  const handleAddToCart = () => null;
+
+  const handleRemoveFromCart = () => null;
+
+  if (isLoading) return <LinearProgress />;
+  if (error) return <div>Something went wrong...</div>
+
   return <div className="App">Start</div>;
 };
 
